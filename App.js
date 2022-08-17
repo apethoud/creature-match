039206193090499e8bb218c840e2008b83d8c798
Andrew Pethoud <andrewpethoud@gmail.com>
2022-08-17 14:34:20 -0700

    feat(tile grid): styled background

--- App.js
+++ App.js
@@ -18,7 +18,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#ecf4f9",
     alignItems: "center",
     justifyContent: "center",
   },
